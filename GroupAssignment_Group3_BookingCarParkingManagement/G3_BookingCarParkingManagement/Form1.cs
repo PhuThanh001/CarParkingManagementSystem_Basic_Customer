0e26166525ca69bc09c82aa4fc057566ee4f4723
baemgao <bhduc264@gmail.com>
2023-08-17 15:15:47 +0700

    new solution

---
+++ GroupAssignment_Group3_BookingCarParkingManagement/G3_BookingCarParkingManagement/Form1.cs
@@ -0,0 +1,10 @@
+namespace G3_BookingCarParkingManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
